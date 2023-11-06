9506968252ca023b25852c2337adb4803247d01f
Pankaj Parkar <parkarpankaj3@gmail.com>
2023-11-06 18:51:14 +0800

    Added deferred view example

--- src/app/components/deferrable-views/deferrable-views.component.ts
+++ src/app/components/deferrable-views/deferrable-views.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SignalComponent } from '../signal/signal.component';
 
 @Component({
   selector: 'af-deferrable-views',
   standalone: true,
-  imports: [CommonModule],
+  imports: [SignalComponent],
   templateUrl: './deferrable-views.component.html',
   styleUrl: './deferrable-views.component.scss'
 })
 export class DeferrableViewsComponent {
-
+  isVisible = signal(false);
 }
