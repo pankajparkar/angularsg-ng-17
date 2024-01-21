fad27844a2b916bb5aa2b1ee11897613eff93489
bhuwan <bkspoudel@gmail.com>
2024-01-21 19:54:34 +0100

    feat: fix contact form

---
+++ src/app/shared/contact/contact.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'af-contact',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.scss'
+})
+export class ContactComponent {
+
+}
